1eda187a3cc22d0fd40ba242d24e70f6dba1aa4d
Eric Wang <eric3691215@gmail.com>
2022-10-15 17:00:18 -0400

    quetsions page

--- testhtml.py
+++ testhtml.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("response.html")
 
 @app.route("/register")
 def register():
