35b5594b97775296c790970217920374c52982e9
Eric Wang <eric3691215@gmail.com>
2022-10-15 11:17:41 -0400

    login and register buttons

--- main.py
+++ main.py
@@ -1,48 +1,18 @@
-import flask
-import firebase, db from firebase
+from flask import Flask, redirect, url_for, render_template
 
-def create_app():
-    app = flask.Flask(__name__)
-    @app.route('/home')
-    def home():
-        return 'Welcome to Pairitas'
-    @app.route('/register', methods=('GET', 'POST'))
-    def register():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            error = None
+app = Flask(__name__)
 
-            if not username:
-                error = 'Username is required.'
-            elif not password:
-                error = 'Password is required.'
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-            if error is None:
-                data = {"password": password, "sleep", "noise": 0, "roommate_num": 0, "phil_response": 0}
-                db.child("users").child(username).set(data)
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-            flash(error)
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
-        return render_template('auth/register.html')
-    @bp.route('/login', methods=('GET', 'POST'))
-    def login():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            user = db.child("users")
-
-            if user is None:
-                error = 'Incorrect username.'
-            elif not check_password_hash(user['password'], password):
-                error = 'Incorrect password.'
-
-            if error is None:
-                session.clear()
-                session['user_id'] = user['id']
-                return redirect(url_for('index'))
-
-            flash(error)
-
-        return render_template('auth/login.html')
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=2000, debug=True)
